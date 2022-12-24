86830a4c53d4111784609c62c2c59bbc3ef1d63c
Stanislav Tkachenko <infastiko@gmail.com>
2022-12-25 00:20:54 +0400

    added Final_Task_Programm

---
+++ Final_Task_Programm/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+
+Console.Write("Введите желаемую длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string[] array = FillArray(num);
+Console.WriteLine();
+PrintArray(array);
+string[] newArray = new string[array.Length];
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        {
+        newArray[count] = array[i];
+        count++;
+        }
+}
+Console.WriteLine();
+PrintArray(newArray);
+
+string[] FillArray(int size)
+{
+    string[] arr = new string[size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Введите желаемый элемент массива: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
